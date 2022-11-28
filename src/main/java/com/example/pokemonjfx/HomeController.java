39bb116c7be3b17460b8ff77d6ccd36d99522853
Jaime Romero <romerocal.jaime@gmail.com>
2022-11-28 09:37:58 +0100

    Initial Preparations

--- src/main/java/com/example/pokemonjfx/HomeController.java
+++ src/main/java/com/example/pokemonjfx/HomeController.java
@@ -3,7 +3,7 @@ package com.example.pokemonjfx;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
-public class HelloController {
+public class HomeController {
     @FXML
     private Label welcomeText;
 
